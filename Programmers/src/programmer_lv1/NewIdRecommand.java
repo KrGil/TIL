f92f9f9209d56c49af25595aa5f0517cd1f7bd76
KrGil <kanjjang89@naver.com>
2021-11-19 20:56:35 +0900

    진행중

--- Programmers/src/programmer_lv1/NewIdRecommand.java
+++ Programmers/src/programmer_lv1/NewIdRecommand.java
@@ -32,21 +32,42 @@ public class NewIdRecommand {
                 new_id = new_id.toLowerCase();
             //2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
                 String pattern = "([^a-z0-9\\-_.])*";
-                String test = "Ae32+-,_-.asd";
-                String arr = test.replaceAll(pattern, "");
+                String test = "a";
+                new_id = test.replaceAll(pattern, "");
 //                Arraysffer sb = new StringBuffer();
-//                Arrays.stream(arr).forEach(a -> sb.append(a));
-                System.out.println(arr);
+//                Arrays.stream(arr).forEach(a -> sb.append(a));r
+                System.out.println(new_id);
 
             //3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
 
             //4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
                 String patternDot = "[.](.*)[.]";
-
+                if(new_id.startsWith("."))
+                    new_id = new_id.substring(1);
+                if(new_id.endsWith("."))
+                    new_id = new_id.substring(0, new_id.length()-1);
+                System.out.println(new_id);
             //5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+                if("".equals(new_id)){
+                    new_id = "a";
+                }
             //6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
-            //     만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+                if(new_id.length() >= 16){
+                    new_id = new_id.substring(0, 15);
+                    System.out.println(new_id.length());
+                    System.out.println(new_id);
+                }
             //7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+            if(new_id.length() <= 2){
+                System.out.printf("here : %s %n",new_id.length());
+                while(new_id.length() < 3){
+                    new_id += new_id.substring(new_id.length()-1, new_id.length());
+                    System.out.println("aa : "+new_id.substring(new_id.length()-1, new_id.length()));
+                };
+            }
+            new_id = new_id.trim();
+            System.out.println(new_id.length());
+            System.out.println(new_id);
 
 //            String pattern = "^[0-9]*$"; // 숫자 만
 //            String val = "123456789"; // 대상문자열
