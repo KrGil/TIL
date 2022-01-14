399951d389a73e0ff76f3d19f108f0640b5aac09
KrGil <kanjjang89@naver.com>
2022-01-14 22:19:58 +0900

    something changed

--- Programmers/bin/src/programmer_lv1/ArrAverage.java
+++ Programmers/bin/src/programmer_lv1/ArrAverage.java
@@ -6,11 +6,6 @@ public class ArrAverage {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4};
         System.out.println(Arrays.stream(arr).average().getAsDouble());
-        String a = "asdf";
-        char[] d = a.toCharArray();
-
-        String[] b  =a.split("");
-        System.out.println("b = " + d.toString());
     }
 
 }
