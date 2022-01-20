598a3c4ad2da31043f0005e1d661a7af6014a98d
Eisen <kanjjang89@naver.com>
2022-01-20 13:39:56 +0900

    leetCode completed

--- Programmers/bin/src/leetCode/PalindromeNumber.java
+++ Programmers/bin/src/leetCode/PalindromeNumber.java
@@ -3,7 +3,7 @@ package leetCode;
 public class PalindromeNumber {
     public static void main(String[] args) {
         PalindromeNumber pn = new PalindromeNumber();
-        System.out.println(pn.isPalindrome(12221));
+        System.out.println(pn.rpIsPalindrome(-12231));
     }
     public boolean isPalindrome(int x){
         String[] sarr = String.valueOf(x).split("");
@@ -23,5 +23,17 @@ public class PalindromeNumber {
             }
         }
         return true;
+    }
+    public boolean rpIsPalindrome(int x){
+        int lastDigit = 0;
+        int tmp = x;
+        int reversedNum = 0;
+        while (tmp != 0) {
+            lastDigit = tmp % 10;
+            tmp = tmp / 10;
+            reversedNum = (reversedNum * 10) + lastDigit;
+        }
+        System.out.println("reversedNum = " + reversedNum);
+        return reversedNum==x && x > 0?true:false;
     }
 }
