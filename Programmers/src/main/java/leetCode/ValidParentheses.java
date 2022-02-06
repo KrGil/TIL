0e942b01e9d1ba6e3ddc17e53bf68aa5838df3db
Eisen <kanjjang89@naver.com>
2022-02-06 21:54:56 +0900

    validParentheses added

--- Programmers/src/main/java/leetCode/ValidParentheses.java
+++ Programmers/src/main/java/leetCode/ValidParentheses.java
@@ -2,14 +2,19 @@ package leetCode;
 
 import com.tistory.eisen.Eisen;
 
+import java.util.Arrays;
+
 public class ValidParentheses {
     public static void main(String[] args) {
         ValidParentheses validParentheses = new ValidParentheses();
-        String s = "()";
-        validParentheses.solution(s);
+        String s = "(]";
+        System.out.println("validParentheses = " + validParentheses.solution(s));
     }
 
-    private void solution(String s) {
+    private boolean solution(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
         char sBracketIn = '(';
         char sBracketOut =')';
         char mBracketIn = '{';
@@ -17,10 +22,60 @@ public class ValidParentheses {
         char bBracketIn = '[';
         char bBracketOut =']';
         char[] c = s.toCharArray();
-        for (char o : c) {
-            if(o == '('){
-
-            }
+        int sCntStart = 0;
+        int mCntStart = 0;
+        int bCntStart = 0;
+        int sCntEnd = 0;
+        int mCntEnd = 0;
+        int bCntEnd = 0;
+        // 반을 나눠야함.
+        // 반 나눈 후 나오는 순서에 유의해야함. ({ }),
+//        boolean isRight = false;
+//        for (int i = 0; i < c.length; i++) {
+//            char c1 = c[i];
+//            if (c[i] == '(') {
+//                if (c[i + 1] != ')') {
+//                    return false;
+//                } else{
+//                    isRight = true;
+//                    continue;
+//                }
+//            }
+//            if (c[i] == '{') {
+//                if (c[i + 1] != '}') {;
+//                    return false;
+//                } else {
+//                    isRight = true;
+//                    continue;
+//                }
+//            }
+//            if (c[i] == '[') {
+//                if (c[i + 1] != ']') {;
+//                    return false;
+//                } else {
+//                    isRight = true;
+//                    continue;
+//                }
+//            }
+//        }
+        // 바로 뒤가 아닌 {[]} 이러한 경우도 넣어 주어야함.
+        // reverse. 앞의 반틈과 뒤의 반틈.
+        StringBuffer sb = new StringBuffer(s);
+        System.out.println("sb = " + sb.charAt(0));
+        String[] sarrFirst = new String[s.length()/2];
+        String[] sarrSecond = new String[s.length()/2];
+        System.out.println("s.length() = " + s.length());
+        System.out.println("s.length()/2 = " + s.length()/2);
+        for (int i = 0; i < s.length()/2; i++) {
+            System.out.println("i = " + i);
+            System.out.println("String.valueOf(sb.charAt(i)) = " + String.valueOf(sb.charAt(i)));
+            sarrFirst[i] = String.valueOf(sb.charAt(i));
+            System.out.println("sarrFirst = " + sarrFirst[0]);
+            System.out.println("sarrFirst = " + Arrays.toString(sarrFirst));
+            sarrSecond[i] = String.valueOf(sb.charAt(s.length()-(i+1)));
         }
+        Eisen.printArray(sarrFirst);
+        Eisen.printArray(sarrSecond);
+        return false;
     }
 }
