cafd3de7874acbcec8c86de17621583a86ba5a0e
Eisen <kanjjang89@naver.com>
2022-02-20 17:00:43 +0900

    leetcode addBinary added

--- Programmers/src/main/java/leetCode/AddBinary.java
+++ Programmers/src/main/java/leetCode/AddBinary.java
@@ -1,12 +1,28 @@
-package leetCode;/**
+package leetCode;
+
+import com.tistory.eisen.Eisen;
+
+/**
  * packageName :  leetCode
  * fileName : AddBinary
  * author :  eisen
- * date : 2022/02/20 
+ * date : 2022/02/20
  * description :
  * ===========================================================
  * DATE                 AUTHOR                NOTE
  * -----------------------------------------------------------
  * 2022/02/20                eisen             최초 생성
- */public class AddBinary {
+ */
+public class AddBinary {
+    public static void main(String[] args) {
+        String a = "11r";
+        String b = "1";
+        // a + b = 10;
+        // if x + y = 2 ---> 10
+
+        char[] aArray = a.toCharArray();
+        char[] bArray = b.toCharArray();
+
+         
+    }
 }
