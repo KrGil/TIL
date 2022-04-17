fa3a5b2cee024b03111fecb7fc7581106b654bf1
Eisen <kanjjang89@naver.com>
2022-04-17 15:14:13 +0900

    Add: leetCode 이진탐색 코스 시작

--- Programmers/src/main/java/leetCode/BinarySearch.java
+++ Programmers/src/main/java/leetCode/BinarySearch.java
@@ -1,12 +1,34 @@
-package leetCode;/**
+package leetCode;
+
+/**
  * packageName :  leetCode
  * fileName : BinarySearch
  * author :  eisen
- * date : 2022/04/16 
+ * date : 2022/04/16
  * description :
  * ===========================================================
  * DATE                 AUTHOR                NOTE
  * -----------------------------------------------------------
  * 2022/04/16                eisen             최초 생성
- */public class BinarySearch {
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+    /*
+        nums = [-1,0,3,5,9,12], target = 9
+        nums = [-1,0,3,5,9,12], target = 2
+     */
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        search(nums, target);
+
+    }
+    public static int search(int[] nums, int target) {
+        int[] left;
+        int[] right;
+        int mid = nums.length / 2;
+
+
+        return -1;
+    }
 }
+
