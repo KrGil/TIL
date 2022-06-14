0fbd3bd4e4acc668ac1077ff0514fbb3ca86b07a
Eisen <kanjjang89@naver.com>
2022-06-14 12:39:33 +0900

    Modify: 내용 추가

---
+++ Programmers/src/test/java/leetCode/fun_with_array/MaxConsecutiveOnesTest.java
@@ -0,0 +1,33 @@
+package leetCode.fun_with_array;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * packageName :  leetCode.fun_with_array
+ * fileName : MaxConsecutiveOnesTest
+ * author :  eisen
+ * date : 2022/06/11
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2022/06/11                eisen             최초 생성
+ */
+
+class MaxConsecutiveOnesTest {
+
+
+    MaxConsecutiveOnes maxConsecutiveOnes;
+
+    @Test
+    void findMaxConsecutiveOnes() {
+        maxConsecutiveOnes.findMaxConsecutiveOnes(new int[]{0,1,1,0,1});
+    }
+
+    @Test
+    void find() {
+    }
+}
